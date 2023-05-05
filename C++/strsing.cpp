f20c0e95b2c808fbc65e9b6aac5c6ea685d02208
date3k2 <nguyenduydat047@gmail.com>
2023-05-06 02:35:09 +0700

    code

---
+++ C++/strsing.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    string s = "1242";
+    cout << s;
+    return 0;
+}
