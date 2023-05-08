067fa2daedf41aab043ef4663fe76162390d75b5
date3k2 <nguyenduydat047@gmail.com>
2023-05-08 10:47:30 +0700

    code

--- C++/template.cpp
+++ C++/template.cpp
@@ -2,32 +2,19 @@
 #define fo(i, l, h) for (int i = l; i <= h; ++i)
 #define ll long long
 using namespace std;
-int t;
 int main()
 {
-    // freopen("test.inp","r",stdin);
-    // freopen("test.out","w",stdout);
-    // ios_base::sync_with_stdio(false);
-    // cin.tie(NULL);
-    // cin >> t;
-    // while (t--)
-    // {
-    //     // int n;
-    //     // cin >> n;
-    //     cout << t;
-    // }
-    char vowels[] = {'a', 'e', 'i', 'o', 'u'};
-    // check if a character is a vowel
-    char ch = 'I';
-    auto x = find(begin(vowels), end(vowels), tolower(ch))-begin(vowels);
-    string s = "gafas";
-    reverse(s.begin(), s.end());
-    cout << s;
-    for (auto &&i : s)
+    int t;
+    freopen("test.inp", "r", stdin);
+    freopen("test.out", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cin >> t;
+    while (t--)
     {
-        cout<<i<<" ";
+        // int n;
+        // cin >> n;
+        cout << t;
     }
-    
-
     return 0;
 }
