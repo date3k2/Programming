568d55bfe2fdc77e3beb5f9be1f2a02316a12b64
date3k2 <nguyenduydat047@gmail.com>
2023-10-10 02:14:44 +0700

    code củng 🐧😷

--- C++/generatepermutation.cpp
+++ C++/generatepermutation.cpp
@@ -1,31 +1,36 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define fo(i,l,h) for(int i=l;i<=h;++i)
+#define fo(i, l, h) for (int i = l; i <= h; ++i)
 int n;
 bool chosen[100];
 vector<int> pm;
 void search()
 {
-if (pm.size()==n) 
-{ for (auto i: pm) cout<<i<<" ";
-  cout<<endl;}
-else {
-  fo(i,1,n)
+  if (pm.size() == n)
   {
-      if (chosen[i]==true) continue;
-      chosen[i]=true;
+    for (auto i : pm)
+      cout << i << " ";
+    cout << endl;
+  }
+  else
+  {
+    fo(i, 1, n)
+    {
+      if (chosen[i] == true)
+        continue;
+      chosen[i] = true;
       pm.push_back(i);
       search();
-      chosen[i]=false;
+      chosen[i] = false;
       pm.pop_back();
-
+    }
   }
 }
-}
-int main() {
-ios_base::sync_with_stdio(0);
-cin.tie(0);
-cin>>n;
-search();
-return 0;
+int main()
+{
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  cin >> n;
+  search();
+  return 0;
 }
